86610116c0c6d90fe5126ae014695b69a1af2c4c
k4-100 <k4-100@protonmail.com>
2023-02-04 17:14:17 +0100

    Initial commit

---
+++ src/components/PlaceHolder.tsx
@@ -0,0 +1,14 @@
+import { Box, Typography } from "@mui/material"
+
+
+const PlaceHolder = () =>{
+  return(
+  <Box>
+    <Typography>
+      Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+    </Typography>
+  </Box>
+  )
+}
+
+export default PlaceHolder
