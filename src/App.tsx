8e6f53e1d6ffcb7e2d3c9b28ce4a56d9d6a773b2
k4-100 <k4-100@protonmail.com>
2023-02-04 18:58:54 +0100

    Purged bunch of stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
+import { Box } from '@mui/system'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 import PlaceHolder from './components/PlaceHolder'
 
 
@@ -8,36 +7,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <PlaceHolder>
-      </PlaceHolder>
-
-      <div>
-        <h2>
-          close
-        </h2>
-        <h1>
-        </h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Box>
+      <PlaceHolder />
+    </Box>
   )
 }
 
