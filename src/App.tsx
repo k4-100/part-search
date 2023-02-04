e6fefcd4d4d09610be53415ea8ab5a2e92afe291
k4-100 <k4-100@protonmail.com>
2023-02-04 21:59:21 +0100

    Added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
-import { Box } from '@mui/system'
-
-
+import { Box, CssBaseline, ThemeProvider, createTheme, AppBar, Toolbar, IconButton, Typography, Button } from '@mui/material'
+import { Menu, Adb, GitHub, LinkedIn } from '@mui/icons-material'
 const darkTheme = createTheme({
   palette: {
     mode: "dark"
@@ -14,7 +12,28 @@ const App = () => {
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <Box>
-        sad
+        <Box sx={{ flexGrow: 1 }} >
+          <AppBar position="static">
+            <Toolbar>
+              <Adb />
+              <Typography 
+                variant="h5" 
+                component="div" 
+                sx={{ flexGrow: 1, textAlign: "center" }}
+              >
+                part-search
+              </Typography>
+              <Box>
+                <IconButton>
+                  <GitHub />
+                </IconButton>
+                <IconButton>
+                  <LinkedIn />
+                </IconButton>
+              </Box>
+            </Toolbar>
+          </AppBar>
+        </Box>
       </Box>
     </ThemeProvider>
   )
