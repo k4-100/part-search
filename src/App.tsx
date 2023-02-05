d9fe22f34515bfd4f59ed8dccc12990e7be55214
k4-100 <k4-100@protonmail.com>
2023-02-05 16:12:36 +0100

    implemented nerd font as a main font for the website

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import { Box, CssBaseline, ThemeProvider, createTheme, AppBar, Toolbar, IconButton, Typography, Button } from '@mui/material'
 import { Menu, Adb, GitHub, LinkedIn } from '@mui/icons-material'
+import mainWebsiteFont from './assets/fonts/agave_regular_Nerd_Font_Complete_Mono.ttf'
+
+
 const darkTheme = createTheme({
   palette: {
     mode: "dark"
-  }
+  },
+typography: {
+    fontFamily: 'agave',
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: 'agave';
+          font-style: normal;
+          font-display: swap;
+          font-weight: 400;
+          src: url(${mainWebsiteFont});
+          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+        }
+      `,
+    },
+  },
 })
+
+// family: agave Nerd Font Mono
 
 
 const App = () => {
