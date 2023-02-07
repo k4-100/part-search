da7e0bfc633c19dfbd6867da5053e418ee397ae1
k4-100 <k4-100@protonmail.com>
2023-02-07 22:31:53 +0100

    working with border and opacity of a single search record

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,4 +1,5 @@
-import { Box, Link, Typography } from '@mui/material'
+import { Box, Paper, Link, Typography } from '@mui/material'
+import { blueGrey, grey } from "@mui/material/colors";
 import React from 'react'
 
 import placeholderImg from '../assets/images/placeholders/1-karty-graficzne-box.webp'
@@ -6,12 +7,16 @@ import placeholderImg from '../assets/images/placeholders/1-karty-graficzne-box.
 
 const SearchResults = () =>{
   return(
-    <Box sx={{
+    <Box
+      // elevation={2}
+      sx={{
       // background: "blue",
       width:"clamp(270px, 55vw, 900px)",
       mx: "auto",
       p: "10px",
-      border:"1px solid grey"
+      pl: 2,
+      border:`1px solid ${grey[800]}`,
+      borderRadius: "5px"
     }}>
       <Box sx={{
         display: "flex",
@@ -27,15 +32,17 @@ const SearchResults = () =>{
               nvidia geforce rtx 3050 gigabyte 6GB
             </Link>
           </Typography>
-          <Typography variant="h4" gutterBottom>
+          <Typography variant="h4">
             $100.99
           </Typography>
         </Box>
         <img src={placeholderImg} alt="product image" style={{
           display: "block",
-          width:"50%",
-          marginLeft: "18px"
+          width:"40%",
+          marginLeft: "18px",
+          opacity: 0.9
         }} />
+  
       </Box>
     </Box>
   )
