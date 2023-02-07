995561f7a2daf3812af5c7869181cd4eddbc17f0
k4-100 <k4-100@protonmail.com>
2023-02-07 22:15:55 +0100

    Started implementing single record

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,17 +1,42 @@
-import { Typography } from '@mui/material'
-import { Box } from '@mui/system'
+import { Box, Link, Typography } from '@mui/material'
 import React from 'react'
+
+import placeholderImg from '../assets/images/placeholders/1-karty-graficzne-box.webp'
+
 
 const SearchResults = () =>{
   return(
     <Box sx={{
       // background: "blue",
       width:"clamp(270px, 55vw, 900px)",
-      mx: "auto"
+      mx: "auto",
+      p: "10px",
+      border:"1px solid grey"
     }}>
-      <Typography>
-        Search Results: 
-      </Typography>
+      <Box sx={{
+        display: "flex",
+        flex: 1
+      }}> 
+        <Box sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between"
+        }}>
+          <Typography variant="h6" gutterBottom>
+            <Link>
+              nvidia geforce rtx 3050 gigabyte 6GB
+            </Link>
+          </Typography>
+          <Typography variant="h4" gutterBottom>
+            $100.99
+          </Typography>
+        </Box>
+        <img src={placeholderImg} alt="product image" style={{
+          display: "block",
+          width:"50%",
+          marginLeft: "18px"
+        }} />
+      </Box>
     </Box>
   )
 }
