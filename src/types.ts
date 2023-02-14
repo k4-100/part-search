e30f79d49a89d02f7cca2b3d3ef411f43623d69f
k4-100 <k4-100@protonmail.com>
2023-02-14 21:05:29 +0100

    Created type for SearchRecords

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,21 @@
-export {
 
+type SearchRecordOptions = {
+  isDrawerRecord: boolean;
+
+}
+
+export class SearchRecord{
+  name: string;
+  link: string;
+  price: number;
+  imageUrl: string;
+  options: SearchRecordOptions;
+
+  constructor(name: string, link: string, price: number, imageUrl: string, options: SearchRecordOptions = { isDrawerRecord: false } ){
+    this.name = name;
+    this.link = link;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.options = options;
+  }
 }
