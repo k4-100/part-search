d2272fff4f04a065a489164c7792f66f8f498d1b
k4-100 <k4-100@protonmail.com>
2023-02-15 18:47:13 +0100

    Reworked *Drawer.tsx components

--- src/components/AdviceDrawer.tsx
+++ src/components/AdviceDrawer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
@@ -23,154 +23,113 @@ import { Pagination, Navigation } from "swiper";
 
 
 
-type Anchor = 'top' | 'left' | 'bottom' | 'right';
 
 
 const drawerWidth = 550
 
 const AdviceDrawer = () => {
-  const [state, setState] = React.useState({
-    top: false,
-    left: false,
-    bottom: false,
-    right: false,
-  });
-  
   const [drawerToggled, setDrawerToggled] = useState(false)
 
 
-  const drawerRef = useRef(null);
-
-
-  useEffect(()=>{
-    if (drawerRef.current !== null){
-      console.log(drawerRef)
-    }
-  });
-
-
-
-
-  const toggleDrawer =
-    (anchor: Anchor, open: boolean) =>
-    (event: React.KeyboardEvent | React.MouseEvent) => {
-      if (
-        event.type === 'keydown' &&
-        ((event as React.KeyboardEvent).key === 'Tab' ||
-          (event as React.KeyboardEvent).key === 'Shift')
-      ) {
-        return;
-      }
-
-      
-      setDrawerToggled( !drawerToggled );
-      setState({ ...state, [anchor]: open });
-    };
-
+  const toggleDrawer = () => setDrawerToggled( !drawerToggled )
 
   return (
     <Box>
-      {(['left'] as const).map((anchor) => (
-        <React.Fragment key={anchor}>
-            <Button 
-              variant="contained"
-              color={ drawerToggled ? "error" : "info"}
-              onClick={toggleDrawer(anchor, true)}
-              sx={{
-                position: "absolute",
-                zIndex: 1300,
-                height: "50px",
-                top: "15px",
-                mr: drawerToggled ? `${drawerWidth}px` : 0,
-                right: 0,
-                p: 0,
-                transitionProperty: "margin, background-color",
-                transitionDuration: "220ms, 700ms"
-              }}
-            >
-              {
-                drawerToggled 
-                ? <>
-                  <Close 
-                    sx={{
-                      fontSize: "40px"
-                    }}
-                  />
-                </>
-                : <>
-                  <Info
-                    sx={{
-                      fontSize: "40px"
-                    }}
-                  />
-                </>
-              }
-
-            </Button>
-            <Drawer
-            // ref={drawerRef}
-            open={drawerToggled}
-            anchor="right"
-            // open
-            onClose={toggleDrawer(anchor, false)}
-            id="21321"
-          >
-            <Box sx={{
-              height: 1,
-              background: grey[50]
-            }}>
-              <Box
+        <Button 
+          variant="contained"
+          color={drawerToggled ? "error" : "info"}
+          onClick={toggleDrawer}
+          sx={{
+            position: "absolute",
+            zIndex: 1300,
+            height: "50px",
+            top: "15px",
+            mr: drawerToggled ? `${drawerWidth-15}px` : 0,
+            right: 0,
+            p: 0,
+            transitionProperty: "margin, background-color",
+            transitionDuration: "220ms, 700ms"
+          }}
+        >
+          {
+            drawerToggled 
+            ? <>
+              <Close 
                 sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  height: "100%",
-                  width: `${drawerWidth}px`,
-                  "& .swiper-button-prev":{
-                    top: "5vh",
-                    left: "50%",
-                    transform: "translateX(-50%) rotateZ(90deg)",
-                  },
-                  "& .swiper-button-next":{
-                    top: "95vh",
-                    left: "50%",
-                    transform: "translateX(-50%) rotateZ(90deg)",
-                  },
-                  "& .swiper-pagination-fraction":{
-                    mb: 10,
-
-                  }
+                  fontSize: "40px"
                 }}
+              />
+            </>
+            : <>
+              <Info
+                sx={{
+                  fontSize: "40px"
+                }}
+              />
+            </>
+          }
+
+        </Button>
+        <Drawer
+        open={drawerToggled}
+        anchor="right"
+        onClose={toggleDrawer}
+        id="21321"
+      >
+        <Box sx={{
+          height: 1,
+          background: grey[50]
+        }}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100%",
+              width: `${drawerWidth}px`,
+              "& .swiper-button-prev":{
+                top: "5vh",
+                left: "50%",
+                transform: "translateX(-50%) rotateZ(90deg)",
+              },
+              "& .swiper-button-next":{
+                top: "95vh",
+                left: "50%",
+                transform: "translateX(-50%) rotateZ(90deg)",
+              },
+              "& .swiper-pagination-fraction":{
+                mb: 10,
+
+              }
+            }}
+          >
+            <>
+              <Swiper
+                direction={"vertical"}
+                pagination={{
+                  clickable: true,
+                  type: "fraction"
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
               >
-                <>
-                  <Swiper
-                    direction={"vertical"}
-                    pagination={{
-                      clickable: true,
-                      type: "fraction"
-                    }}
-                    navigation={true}
-                    modules={[Pagination, Navigation]}
-                    className="mySwiper"
-                  >
-                  {
-                    [null,null,null,null].map( 
-                      (_,i) => <SwiperSlide key={i}>
-                        <Box sx={{
-                          mx: "15px"
-                        }}>
-                          Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
-                        </Box>
-                    </SwiperSlide>
-                    )
-                  }
-                  </Swiper>
-                </>
-              </Box>
-            </Box>
-          </Drawer>
-        </React.Fragment>
-      ))}
+              {
+                [null,null,null,null].map( 
+                  (_,i) => <SwiperSlide key={i}>
+                    <Box sx={{
+                      mx: "15px"
+                    }}>
+                      Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
+                    </Box>
+                </SwiperSlide>
+                )
+              }
+              </Swiper>
+            </>
+          </Box>
+        </Box>
+      </Drawer>
     </Box>
   );
 }
--- src/components/RecordDrawer.tsx
+++ src/components/RecordDrawer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
@@ -7,126 +7,84 @@ import {grey} from '@mui/material/colors';
 import { MoreVert, Close} from '@mui/icons-material';
 import SearchResult from './SearchResult';
 
-type Anchor = 'top' | 'left' | 'bottom' | 'right';
 
 
 const drawerWidth = 550
 
 const RecordDrawer = () => {
-  const [state, setState] = React.useState({
-    top: false,
-    left: false,
-    bottom: false,
-    right: false,
-  });
-  
   const [drawerToggled, setDrawerToggled] = useState(false)
 
 
-  const drawerRef = useRef(null);
-
-
-  useEffect(()=>{
-    if (drawerRef.current !== null){
-      console.log(drawerRef)
-    }
-  });
-
-
-
-
-  const toggleDrawer =
-    (anchor: Anchor, open: boolean) =>
-    (event: React.KeyboardEvent | React.MouseEvent) => {
-      if (
-        event.type === 'keydown' &&
-        ((event as React.KeyboardEvent).key === 'Tab' ||
-          (event as React.KeyboardEvent).key === 'Shift')
-      ) {
-        return;
-      }
-
-      
-      setDrawerToggled( !drawerToggled );
-      setState({ ...state, [anchor]: open });
-    };
-
+  const toggleDrawer = () => setDrawerToggled( !drawerToggled )
 
   return (
     <Box>
-      {(['left'] as const).map((anchor) => (
-        <React.Fragment key={anchor}>
-            <Button 
-              variant="contained"
-              color={ drawerToggled ? "error" : "success"}
-              onClick={toggleDrawer(anchor, true)}
-              sx={{
-                position: "absolute",
-                zIndex: 1300,
-                height: "50px",
-                top: "15px",
-                ml: drawerToggled ? `${drawerWidth}px` : 0,
-                p: 0,
-                transitionProperty: "margin, background-color",
-                transitionDuration: "220ms, 700ms"
-              }}
-            >
-              {
-                drawerToggled 
-                ? <>
-                  <Close 
-                    sx={{
-                      fontSize: "40px"
-                    }}
-                  />
-                </>
-                : <>
-                  <MoreVert 
-                    sx={{
-                      fontSize: "40px"
-                    }}
-                  />
-                </>
-              }
-
-            </Button>
-            <Drawer
-            // ref={drawerRef}
-            open={drawerToggled}
-            // open
-            onClose={toggleDrawer(anchor, false)}
-            id="21321"
-          >
-            <Box sx={{
-              height: 1,
-              background: grey[50]
-            }}>
-              <Paper
-                elevation={20}
-                square
+        <Button 
+          variant="contained"
+          color={drawerToggled ? "error" : "success"}
+          onClick={toggleDrawer}
+          sx={{
+            position: "absolute",
+            zIndex: 1300,
+            height: "50px",
+            top: "15px",
+            ml: drawerToggled ? `${drawerWidth}px` : 0,
+            p: 0,
+            transitionProperty: "margin, background-color",
+            transitionDuration: "220ms, 700ms"
+          }}
+        >
+          {
+            drawerToggled 
+            ? <>
+              <Close 
                 sx={{
-                // mt: 10,
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height:"100px",
-                width: `${drawerWidth}px`,
-              }}>
-                <Typography variant="h2" textAlign="center">
-                  List:
-                </Typography>
-              </Paper>
-              <Box>
-                { 
-                  Array(2).fill(null).map(
-                      (_, i) => <SearchResult key={i} />
-                  )
-                }
-              </Box>
-            </Box>
-          </Drawer>
-        </React.Fragment>
-      ))}
+                  fontSize: "40px"
+                }}
+              />
+            </>
+            : <>
+              <MoreVert 
+                sx={{
+                  fontSize: "40px"
+                }}
+              />
+            </>
+          }
+        </Button>
+        <Drawer
+        open={drawerToggled}
+        onClose={toggleDrawer}
+        id="21321"
+      >
+        <Box sx={{
+          height: 1,
+          background: grey[50]
+        }}>
+          <Paper
+            elevation={20}
+            square
+            sx={{
+            // mt: 10,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height:"100px",
+            width: `${drawerWidth}px`,
+          }}>
+            <Typography variant="h2" textAlign="center">
+              List:
+            </Typography>
+          </Paper>
+          <Box>
+            { 
+              Array(2).fill(null).map(
+                  (_, i) => <SearchResult key={i} />
+              )
+            }
+          </Box>
+        </Box>
+      </Drawer>
     </Box>
   );
 }
